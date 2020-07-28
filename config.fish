accdedec2cd9edb68b89ab8327edb953812d4320
Rohan Deshpande <rohan.deshpande@gmail.com>
2020-07-28 09:31:09 -0400

    Remove zsh, add fish

---
+++ config.fish
@@ -0,0 +1,7 @@
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/usr/local/opt/openssl@1.1"
+
+alias be="bundle exec"
+alias pg_thrive_prod="PGPASSWORD='XAzxdLGdf3w6ptpP' psql -h thrivedata2.cluster-ca3fkbvccdiw.us-east-1.rds.amazonaws.com -U rdeshpande thrive_production"
+alias pg_solana_stage="PGPASSWORD='Ey4VXZGDu8Sszq8RYIDJ' psql -h solana-staging.cplgwlxdm2bb.us-east-1.rds.amazonaws.com -U postgres solana_staging"
+alias pg_solana_prod="PGPASSWORD='DwYMh9USNSM7G7XkApLq5GwVnNJeZ6WD' psql -h solana-production.cplgwlxdm2bb.us-east-1.rds.amazonaws.com -U postgres solana_production"
+alias g="git"
